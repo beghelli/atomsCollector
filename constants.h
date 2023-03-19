8d0365e131ae9498d445b50273e41faa5a372683
Bruno Beghelli <bruno.beghelli@gmail.com>
2023-03-18 21:21:03 -0300

    #42 Fixes default working resolution

--- constants.h
+++ constants.h
@@ -7,7 +7,7 @@ using namespace std;
 const int DEBUG = false;
 const int DEBUG_KEY = SDL_SCANCODE_B;
 const int SCREEN_WIDTH = 1280;
-const int SCREEN_HEIGHT = 768;
+const int SCREEN_HEIGHT = 720;
 const string ASSETS_FOLDER = "assets";
 
 static const string PATH_SEPARATOR(1, filesystem::path::preferred_separator);
